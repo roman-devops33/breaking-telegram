8eb2599f22afad44f0a50b8ca17cb73175197404
Ukropp <55854392+Ukropp@users.noreply.github.com>
2023-03-15 18:10:02 +0300

    fix: session name to client

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ session_string = os.environ['TG_SESSION_STRING']
 api_id = os.environ['TG_API_ID']
 api_hash = os.environ['TG_API_HASH']
 
-client = Client(name = "BrokeTG", api_id = api_id, api_hash = api_hash, session_string = session_string, in_memory = True)
+client = Client(session_name = "BrokeTG", api_id = api_id, api_hash = api_hash, session_string = session_string, in_memory = True)
 
 
 @client.on_raw_update(group=-100)
